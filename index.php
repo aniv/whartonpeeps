92b90054cea8b459c4262928ef5d7aca6e716d84
Ani Vemprala <ani@aniv.info>
2012-06-21 12:03:27 -0400

    front end changes

--- index.php
+++ index.php
@@ -46,6 +46,19 @@ if ($user_id) {
 		<title>WhartonPeeps</title>
 		<script type="text/javascript" src="javascript/bootstrap.min.js"></script>
         <link rel="stylesheet" href="stylesheets/bootstrap.min.css"  type="text/css" />
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-32846016-1']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>
 	</head>
 <body>
     <div id="fb-root"></div>
@@ -91,8 +104,11 @@ if ($user_id) {
 						?>
 				</div>
 			</div>
+
+			<br/>
+
 			<div class="row">
-				<div class="span2">
+				<div class="span2 offset2">
 					<a href="about.php">About</a> | <a href="privacy.php">Privacy</a>
 				</div>
 			</div>
