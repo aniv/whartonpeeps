904ef2f73f6deec2880b2951bd66a687ffed749e
Ani Vemprala <ani@aniv.info>
2012-06-24 23:56:18 -0400

    fb user data - call from page load

--- fb.php
+++ fb.php
@@ -25,23 +25,6 @@
 	    'secret' => AppInfo::appSecret()
 	));
 
-	function GetFacebookPreviews($fbList)
-	{
-		foreach($fbList as $fbUserId)
-		{
-			$fql = "SELECT id, name, url, pic FROM profile WHERE id = $fbUserId";
-			echo $fql;
-			$res = $facebook->api(array('method'=>'fql.query','query'=>$fql));
-			
-			$result = array("profile_id"=>$res[0]['id'], "profile_name"=>$res[0]['name'], "profile_link"=>$res[0]['url'], "profile_photo_link"=>$res[0]['pic']);
-	
-			if (isset($callback))
-				echo $callback . '('. json_encode($result) .')';
-			else
-				echo json_encode($result);
-		}
-	}
-	
 	$action = $_GET['action'];
 	$fbList = $_GET['fbList'];
 	$callback = $_GET['callback'];
@@ -50,7 +33,22 @@
 	{
 		case "getFacebookPreviews":
 			if (isset($fbList))
-				GetFacebookPreviews($fbList);
+			{
+				foreach($fbList as $fbUserId)
+				{
+					$fql = "SELECT id, name, url, pic FROM profile WHERE id = $fbUserId";
+					// echo $fql;
+					$res = $facebook->api(array('method'=>'fql.query','query'=>$fql));
+
+					$result = array("profile_id"=>$res[0]['id'], "profile_name"=>$res[0]['name'], "profile_link"=>$res[0]['url'], "profile_photo_link"=>$res[0]['pic']);
+
+					if (isset($callback))
+						echo $callback . '('. json_encode($result) .')';
+					else
+						echo json_encode($result);
+				}
+				
+			}
 			break;
 	}
 	
